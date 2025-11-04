97d5996f92baee53a13c7634fd68180a3fbbbf98
wlong <109259315+wlong1@users.noreply.github.com>
2025-11-04 00:03:03 -0500

    Added listbox adding and removing

--- js/control.js
+++ js/control.js
@@ -18,4 +18,55 @@ export function initControls(root) {
     btnSkills.addEventListener('click', () => {
         toggleView(panelSkills, panelItems, btnSkills, btnItems);
     });
+
+
+    // Items and skills panel
+    const itemsListSel = document.getElementById('items');
+    const itemsListbox = document.querySelector('.listbox.items');
+    const itemsCounter = document.querySelector('.space-counter');
+    initListSelection(itemsListSel, itemsListbox, itemsCounter, 20);
+
+    const skillsListSel = document.getElementById('skills');
+    const skillsListbox = document.querySelector('.listbox.skills');
+    initListSelection(skillsListSel, skillsListbox, null, 20);
+
 };
+
+
+function initListSelection(listSel, listbox, counterDisplay, maxSpace = 20) {
+    const spaceDisplay = counterDisplay ? counterDisplay.querySelector('#current-count') : null;
+
+    listSel.addEventListener('click', e => {
+        const li = e.target.closest('li');
+        if (!li) return;
+
+        const value = li.dataset.value;
+        const text = li.textContent;
+
+        const existingBtn = listbox.querySelector(`button[value="${value}"]`);
+        if (existingBtn) {
+            existingBtn.remove();
+            updateSpace(spaceDisplay, -1);
+        } else {
+            if (spaceDisplay) {
+                const currentSpace = +spaceDisplay.textContent;
+                if (currentSpace + 1 > maxSpace) return;
+            }
+
+            const btn = document.createElement('button');
+            btn.className = listbox.classList.contains('skills') ? 'list-skill' : 'list-item';
+            btn.value = value;
+            btn.textContent = text;
+            listbox.appendChild(btn);
+
+            updateSpace(spaceDisplay, 1);
+        }
+    });
+}
+
+
+function updateSpace(counter, change) {
+    if (counter) {
+        counter.textContent = +counter.textContent + change;
+    }
+}
